d5643224ab0071103c692b7d7a75149e8f1ef3bc
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-06-29 13:28:45 +0700

    commit

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,7 +1,12 @@
+import CreateJobForm from "@/components/CreateJobForm";
 import React from "react";
 
 function AddJob() {
-  return <div></div>;
+  return (
+    <div>
+      <CreateJobForm />
+    </div>
+  );
 }
 
 export default AddJob;
